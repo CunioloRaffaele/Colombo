dcb98caadc946cf65dc8966e016b4222a9268b07
Cuniolo Raffaele <raffaelecuniolo@gmail.com>
2025-10-29 22:19:09 +0100

    Update env variable deployment to use .env file
    Modified the deployment workflow and ssh-setEnvs.sh script to set environment variables in a .env file at the specified DEPLOY_PATH instead of appending them to .bashrc. Also removed protobuf compilation step from the workflow.

--- deploy/ssh-setEnvs.sh
+++ deploy/ssh-setEnvs.sh
@@ -9,12 +9,21 @@ ENV_NAME="${1:-$ENV_NAME}"        # First argument or env variable
 ENV_VALUE="${2:-$ENV_VALUE}"      # Second argument or env variable
 SSH_HOST="${3:-$SSH_HOST}"        # Third argument or env variable
 SSH_USER="${4:-$SSH_USER}"        # Fourth argument or env variable
+DEPLOY_PATH="${5:-$DEPLOY_PATH}"  # Fifth argument or env variable
+
 
 echo "Setting environment variable $ENV_NAME on $SSH_HOST"
 # Set environment variable on the remote server
 ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST "
-    echo 'Setting environment variable...' && \
-    echo '$ENV_NAME=$ENV_VALUE' >> ~/.bashrc && \
-    source ~/.bashrc && \
-    echo 'Environment variable set successfully.'
+ENV_FILE=$DEPLOY_PATH/.env
+  touch \$ENV_FILE
+  VAR_LINE=\"$ENV_NAME=$ENV_VALUE\"
+  if grep -q \"^$ENV_NAME=\" \$ENV_FILE; then
+    # Aggiorna la variabile se esiste
+    sed -i \"s/^$ENV_NAME=.*/$VAR_LINE/\" \$ENV_FILE
+  else
+    # Aggiungi la variabile se non esiste
+    echo \$VAR_LINE >> \$ENV_FILE
+  fi
+  echo 'Environment variable set in .env file.'
 "
